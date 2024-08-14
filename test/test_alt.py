20a3c03b85091b27de397a82012efbb83e997f63
Tin Lai <tin@tinyiu.com>
2024-08-14 12:39:39 +1000

    black format
    Signed-off-by: Tin Lai <tin@tinyiu.com>

--- test/test_alt.py
+++ test/test_alt.py
@@ -434,6 +434,7 @@ def test_ensure_alt_path(runner, paths, style):
     assert run.out == ""
     assert paths.work.join(filename).read().strip() == "test-data"
 
+
 @pytest.mark.usefixtures("ds1_repo_copy")
 @pytest.mark.parametrize("readonly", [None, "true", "false"])
 def test_template_readonly(runner, yadm_cmd, paths, tst_sys, readonly):
